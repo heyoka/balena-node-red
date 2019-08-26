feb446cda3943ed50d5445a70814a25cc7ef0340
heyoka <erdverbindung@gmx.at>
2019-08-26 11:31:48 +0200

    reduce mqtt reconnect time to 7000

--- app/settings.js
+++ app/settings.js
@@ -28,7 +28,7 @@ module.exports = {
     //uiHost: "127.0.0.1",
 
     // Retry time in milliseconds for MQTT connections
-    mqttReconnectTime: 15000,
+    mqttReconnectTime: 7000,
 
     // Retry time in milliseconds for Serial port connections
     serialReconnectTime: 15000,
